64deb156328212097545089fc0c91b6773727ebd
ArtyomAmirdzhanyans <tyoma_manager@mail.ru>
2022-10-22 19:07:05 +0300

    Server API endpoint test

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const functions = require("firebase-functions");
+const express = require("express");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.get("/api", (req, res) => {
+  console.log("Hello, world!");
+  res.send({
+    now: new Date().getTime(),
+  });
+});
+
+exports.app = functions.region("europe-central2").https.onRequest(app);
