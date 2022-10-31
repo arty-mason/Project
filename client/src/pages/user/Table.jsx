bccc050f6aa19a194aaf84ecc61fc71281a17403
ArtyomAmirdzhanyans <tyoma_manager@mail.ru>
2022-10-31 16:27:10 +0300

    Comment for deploy

--- client/src/pages/user/Table.jsx
+++ client/src/pages/user/Table.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-import { Button, Box, Typography } from "@mui/material";
+import { /* Button, */ Box, Typography } from "@mui/material";
 import styled from "@emotion/styled";
 
 const rows = [
